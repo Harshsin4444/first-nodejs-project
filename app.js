c75f12e877057641d2e6dd7af930c9a23b839b22
Harshsin4444 <singhalharsh4444@gmail.com>
2024-08-24 14:20:07 +0530

    Server using response

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log("Harsh Singhal");
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Page</title></head>');
+    res.write('<body><h1>Welcome to my NodeJs project</h1></body>');
+    res.write('</html>');
+    res.end();
 })
 
 server.listen(4000);
